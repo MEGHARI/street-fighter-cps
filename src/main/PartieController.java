a28dfb96236f5a0616e334c144355f003de7dddc
MEGHARI <meghari.aghiles@yahoo.fr>
2017-04-27 11:58:59 +0200

    maj

---
+++ src/main/PartieController.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import services.EngineService;
+
+public class PartieController implements Initializable {
+	
+	private EngineService engine;
+	@FXML
+	private AnchorPane scene;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+
+	}
+	
+	@FXML
+	public void onKeyPressed(KeyEvent event) {
+		
+	}
+	
+	@FXML
+	public void onKeyReleased(KeyEvent event) {
+		
+	}
+
+}
